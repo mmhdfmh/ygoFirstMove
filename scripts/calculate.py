ef1b7941602940f5a96943acad222c05a5d4f7f5
mmhdfmh <mmhdfmh@gmail.com>
2025-07-18 02:47:04 +0900

    ui 기초, 코어 로직 개선

--- scripts/calculate.py
+++ scripts/calculate.py
@@ -1,57 +1,66 @@
 import math
 from itertools import combinations
+from functools import reduce, lru_cache
 
-def calculate_firstmove_prob(deck: int, draw: int, cards: list[int]) -> float:
-    """
-    덱 N장에서 M장을 뽑을 때, 손에 각 카드가 한 장 이상 뽑힐 확률을 계산합니다.
+def _reduce_rule_max_value(dict1: dict, dict2: dict) -> dict:
+    if dict1 == None:
+        return None
+    ret = dict1.copy()
+    for key, value in dict2.items():
+        ret_value = ret.get(key)
+        if ret_value != None:
+            if ret_value > 0 and value <= 0 or ret_value <= 0 and value > 0:
+                return None
+            ret[key] = max(ret_value, value)
+        else:
+            ret[key] = value
+    return ret
 
-    Args:
-        deck (int): 전체 덱의 매수 (N)
-        draw (int): 뽑을 카드의 수 (M)
-        cards (list[int]): 원하는 카드 각각의 매수 ([X1, X2...])
-
-    Returns:
-        float: 각 카드를 한 장 이상 뽑을 확률 (0 <= P <= 1)
-    """
-    # cards가 iterable이 아니라면 변환
-    try: iter(cards)
-    except: cards = [cards]
-    # 덱보다 뽑을 카드가 많거나 원하는 카드의 종류가 뽑을 카드보다 많은 경우 필터링
-    if deck < draw or draw < len(cards):
-        return 0
+@lru_cache
+def _hand_combination(deck: int, draw: int, cards_tuple: tuple[tuple[str, int]], case_tuple: tuple[tuple[str, int]]) -> int:
+    cards = dict(cards_tuple)
     
-    # 전체 경우의 수와 뽑히지 않은 경우의 수 초기화
-    total_comb_cnt = math.comb(deck, draw)
-    fail_comb_cnt = 0
+    include_case = tuple((k, v) for k, v in case_tuple if v > 0)
+    exclude_case = tuple((k, v) for k, v in case_tuple if v <= 0)
     
-    # 각 종류의 카드를 제외하고 뽑는 경우의 수를 뽑히지 않는 경우의 수에 추가
-    # 중복되어 계산된 부분은 빼줌
-    for i in range(1, len(cards) + 1):
-        for exclude_cards in map(sum, combinations(cards, i)):
-            remaining_deck = deck - exclude_cards
-            term = math.comb(remaining_deck, draw)
-            fail_comb_cnt -= (-1) ** i * term
+    if exclude_case:
+        for card, req in exclude_case:
+            deck -= cards.pop(card, 0)
+        
+        if draw > deck:
+            return 0
+        
+        cards_tuple = tuple(sorted(cards.items()))
+        return _hand_combination(deck, draw, cards_tuple, include_case)
+
+    if not case_tuple:
+        return math.comb(deck, draw)
     
-    # 전체에서 실패를 빼 성공을 반환
-    success_comb_cnt = total_comb_cnt - fail_comb_cnt
-    success_comb_prob = success_comb_cnt / total_comb_cnt
-    return success_comb_prob
+    card, req = case_tuple[0]
+    remain_case = case_tuple[1:]
+    
+    card_cnt = cards.get(card, 0)
+    deck -= card_cnt
+    
+    return sum(
+        math.comb(card_cnt, i) * _hand_combination(deck, draw - i, cards_tuple, remain_case)
+        for i in range(req, min(card_cnt, draw) + 1)
+    )
 
-def calculate_total_prob(cases_prob: list[float]) -> float:
-    """
-    모든 확률 케이스 중 하나 이상이 실현 될 확률을 계산합니다.
-
-    Args:
-        cases_prob (list[float]): 각 케이스가 실현 될 확률 [P1, P2...]
-
-    Returns:
-        float: 모든 케이스 중 하나 이상이 실현 될 확률 (0 <= P_t <= 1)
-    """
-    # 실패 확률 초기화
-    total_fail_prob = 1
-    # 각 케이스가 실패할 확률을 모두 곱함
-    for prob in cases_prob:
-        total_fail_prob *= 1 - prob
-    # 1에서 모두 실패할 확률을 빼 성공 확률을 반환
-    total_success_prob = 1 - total_fail_prob
-    return total_success_prob
+def total_prob(deck: int, draw: int, cards: dict[str, int], cases: list[dict[str, int]]) -> float:
+    total_combination = math.comb(deck, draw)
+    if total_combination == 0:
+        return 0.
+    success_combination = 0
+    
+    cards_tuple = tuple(sorted(cards.items()))
+    for i in range(1, len(cases) + 1):
+        for case_combination in combinations(cases, i):
+            merge_case = reduce(_reduce_rule_max_value, case_combination)
+            
+            if merge_case != None:
+                merge_case_tuple = tuple(sorted(merge_case.items()))
+                success_combination -= (-1)**i * _hand_combination(deck, draw, cards_tuple, merge_case_tuple)
+            
+    success_prob = success_combination / total_combination
+    return success_prob
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 $('#btn').on('click', function() {
+    if(pyodide === null) return;
     $('body').append(`
-        <p>${pyodide.runPython('f"{calculate_firstmove_prob(40, 5, 3):.2%}"')}</p>
+        <p>${pyodide.runPython('f"{total_prob(40, 5, {\'A\': 3}, [{\'A\': 2}]):.2%}"')}</p>
     `)
 });
